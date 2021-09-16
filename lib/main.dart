fa6423a82c57eb5ddba8b6d5fb55b299b693da47
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-17 05:54:00 +0700

    add change notifier provider and consumer

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_provider_v2/application_color.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -12,33 +16,50 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              "Provider State Management",
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AnimatedContainer(
-                  margin: EdgeInsets.all(5),
-                  duration: Duration(milliseconds: 500),
-                  width: 150,
-                  height: 150,
-                  color: Colors.lightBlue,
+        home: ChangeNotifierProvider<ApplicationColor>(
+          create: (BuildContext context) => ApplicationColor(),
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              title: Consumer<ApplicationColor>(
+                builder: (context, state, _) => Text(
+                  "Provider State Management",
+                  style: TextStyle(color: state.color),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(margin: EdgeInsets.all(5), child: Text("AB")),
-                    Switch(value: true, onChanged: (newValue) {}),
-                    Container(margin: EdgeInsets.all(5), child: Text("LB")),
-                  ],
-                )
-              ],
+              ),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Consumer<ApplicationColor>(
+                    builder: (context, state, _) => AnimatedContainer(
+                      margin: const EdgeInsets.all(5),
+                      duration: const Duration(milliseconds: 500),
+                      width: 150,
+                      height: 150,
+                      color: state.color,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                          margin: const EdgeInsets.all(5),
+                          child: const Text("AB")),
+                      Consumer<ApplicationColor>(
+                          builder: (context, state, _) => Switch(
+                              value: state.isLightBlue,
+                              onChanged: (newValue) {
+                                state.isLightBlue = newValue;
+                              })),
+                      Container(
+                          margin: const EdgeInsets.all(5),
+                          child: const Text("LB")),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ));
